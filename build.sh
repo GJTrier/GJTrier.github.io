c454be3368d1554a062a289e5d20795d6f56da78
vidister <David.Weber42@Outlook.com>
2016-07-25 14:27:33 +0200

    Travis test

---
+++ build.sh
@@ -0,0 +1,19 @@
+export REPO="$(pwd | sed s,^/home/travis/build/,,g)"
+echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
+
+#Set git user
+git config --global user.email "David.Weber42@Outlook.com"
+git config --global user.name "Vidisters Travis"
+
+#Set upstream remote
+git remote add upstream https://${GH_TOKEN}@github.com/GJTrier/gruene-jugend-trier.de.git > /dev/null
+
+git fetch -qn upstream > /dev/null
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    git checkout gh-pages
+    nanoc
+    git add -f output/.
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+    git push https://${GH_TOKEN}@github.com/${REPO} gh-pages > /dev/null
+fi
